9c9cc55a06c1ec27e44219b542e099556b7a6254
Rick Chi <wenzhongchi@gmail.com>
2025-03-06 19:18:27 -0500

    fix: update

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -1,74 +1,22 @@
 import {
-  eclipsemainnet,
-  eclipsemainnetAddresses,
-  solanamainnet,
-  solanamainnetAddresses,
+  sepolia,
+  sepoliaAddresses,
+  soneiumtestnet,
+  soneiumtestnetAddresses,
 } from '@hyperlane-xyz/registry';
 import { ChainMap, ChainMetadata } from '@hyperlane-xyz/sdk';
-import { ProtocolType, ExplorerFamily } from '@hyperlane-xyz/utils';
 
 // A map of chain names to ChainMetadata
 // Chains can be defined here, in chains.json, or in chains.yaml
 // Chains already in the SDK need not be included here unless you want to override some fields
 // Schema here: https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/metadata/chainMetadataTypes.ts
 export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
-  solanamainnet: {
-    ...solanamainnet,
-    // SVM chains require mailbox addresses for the token adapters
-    mailbox: solanamainnetAddresses.mailbox,
-    // Including a convenient rpc override because the Solana public RPC does not allow browser requests from localhost
-    rpcUrls: process.env.NEXT_PUBLIC_SOLANA_RPC_URL
-      ? [{ http: process.env.NEXT_PUBLIC_SOLANA_RPC_URL }, ...solanamainnet.rpcUrls]
-      : solanamainnet.rpcUrls,
-  },
-  eclipsemainnet: {
-    ...eclipsemainnet,
-    mailbox: eclipsemainnetAddresses.mailbox,
-  },
   sepolia: {
-    protocol: ProtocolType.Ethereum,
-    chainId: 11155111,
-    domainId: 123123,
-    name: 'sepolia',
-    displayName: 'Sepolia',
-    nativeToken: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-    rpcUrls: [{ http: 'https://api.zan.top/eth-sepolia' }],
-    blockExplorers: [
-      {
-        name: 'MyCustomScan',
-        url: 'https://mycustomchain-scan.com',
-        apiUrl: 'https://api.mycustomchain-scan.com/api',
-        family: ExplorerFamily.Etherscan,
-      },
-    ],
-    blocks: {
-      confirmations: 1,
-      reorgPeriod: 1,
-      estimateBlockTime: 10,
-    },
-    logoURI: '/logo.svg',
+    ...sepolia,
+    mailbox: sepoliaAddresses.mailbox,
   },
-  minato: {
-    protocol: ProtocolType.Ethereum,
-    chainId: 1946,
-    domainId: 1946,
-    name: 'Minato',
-    displayName: 'Minato',
-    nativeToken: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-    rpcUrls: [{ http: 'https://soneium-minato.drpc.org' }],
-    blockExplorers: [
-      {
-        name: 'MyCustomScan',
-        url: 'https://mycustomchain-scan.com',
-        apiUrl: 'https://api.mycustomchain-scan.com/api',
-        family: ExplorerFamily.Etherscan,
-      },
-    ],
-    blocks: {
-      confirmations: 1,
-      reorgPeriod: 1,
-      estimateBlockTime: 10,
-    },
-    logoURI: '/logo.svg',
+  soneiumtestnet: {
+    ...soneiumtestnet,
+    mailbox: soneiumtestnetAddresses.mailbox,
   },
 };
--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,4 +1,4 @@
-import { WarpCoreConfig, TokenStandard } from '@hyperlane-xyz/sdk';
+import { TokenStandard, WarpCoreConfig } from '@hyperlane-xyz/sdk';
 
 // A list of Warp Route token configs
 // These configs will be merged with the warp routes in the configured registry
@@ -7,31 +7,33 @@ export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
     {
       // The ChainName of the token
-      chainName: "sepolia",
+      chainName: 'sepolia',
       // See https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/token/TokenStandard.ts
       standard: TokenStandard.EvmHypCollateral,
       // The token metadata (decimals, symbol, name)
       decimals: 18,
-      symbol: "CT",
-      name: "CustomToken",
+      symbol: 'CT',
+      name: 'CustomToken',
       // The router address
-      addressOrDenom: "0x3213248A0AcED36c91852FFB357B816cc0eCdbcC",
+      addressOrDenom: '0x3213248A0AcED36c91852FFB357B816cc0eCdbcC',
       // The address of the underlying collateral token
-      collateralAddress: "0x76d115B95718Aad543d1687D12e131Bb0B386062",
+      collateralAddressOrDenom: '0x76d115B95718Aad543d1687D12e131Bb0B386062',
       // A path to a token logo image
-      logoURI: "/logos/usdc.png",
+      logoURI: '/logos/usdc.png',
       // The list of tokens this one is connected to
-      connections: [{ token: "ethereum|soneiumtestnet|0x793D5675DE713767f384cD1204e3A39d47a44b29" }],
+      connections: [
+        { token: 'ethereum|soneiumtestnet|0x793D5675DE713767f384cD1204e3A39d47a44b29' },
+      ],
     },
     {
-      chainName: "soneiumtestnet",
+      chainName: 'soneiumtestnet',
       standard: TokenStandard.EvmHypSynthetic,
       decimals: 18,
-      symbol: "CT",
-      name: "CustomToken",
-      addressOrDenom: "0x793D5675DE713767f384cD1204e3A39d47a44b29",
-      logoURI: "/logos/usdc.png",
-      connections: [{ token: "ethereum|sepolia|0x3213248A0AcED36c91852FFB357B816cc0eCdbcC" }],
+      symbol: 'CT',
+      name: 'CustomToken',
+      addressOrDenom: '0x793D5675DE713767f384cD1204e3A39d47a44b29',
+      logoURI: '/logos/usdc.png',
+      connections: [{ token: 'ethereum|sepolia|0x3213248A0AcED36c91852FFB357B816cc0eCdbcC' }],
     },
   ],
   options: {},
