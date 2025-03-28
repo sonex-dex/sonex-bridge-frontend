d345a5364a44f256a73eae4fc669be55342b25cc
Rick Chi <wenzhongchi@gmail.com>
2025-03-27 21:01:07 -0400

    fix: bsc Broccoli

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -33,6 +33,34 @@ export const warpRouteConfigs: WarpCoreConfig = {
       logoURI: 'https://img.sonex.so/bridge/cake.svg',
       connections: [{ token: 'ethereum|bsc|0xD1f5f1613853F5e12ac25cA4B9130d1A71c67807' }],
     },
+    {
+      // The ChainName of the token
+      chainName: 'bsc',
+      // See https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/token/TokenStandard.ts
+      standard: TokenStandard.EvmHypCollateral,
+      // The token metadata (decimals, symbol, name)
+      decimals: 18,
+      symbol: 'Broccoli',
+      name: `CZ'S DOG`,
+      // The router address
+      addressOrDenom: '0xa9aeCd99FEf9bAd545Cc00fc7E53E0a7D3B04778',
+      // The address of the underlying collateral token
+      collateralAddressOrDenom: '0x6d5AD1592ed9D6D1dF9b93c793AB759573Ed6714',
+      // A path to a token logo image
+      logoURI: 'https://img.sonex.so/bridge/cake.svg',
+      // The list of tokens this one is connected to
+      connections: [{ token: 'ethereum|soneium|0xDa5b555598eEe0917dD179F95fF356e300a7e35C' }],
+    },
+    {
+      chainName: 'soneium',
+      standard: TokenStandard.EvmHypSynthetic,
+      decimals: 18,
+      symbol: 'Broccoli',
+      name: `CZ'S DOG`,
+      addressOrDenom: '0xDa5b555598eEe0917dD179F95fF356e300a7e35C',
+      logoURI: 'https://img.sonex.so/bridge/cake.svg',
+      connections: [{ token: 'ethereum|bsc|0xa9aeCd99FEf9bAd545Cc00fc7E53E0a7D3B04778' }],
+    },
   ],
   options: {},
 };
