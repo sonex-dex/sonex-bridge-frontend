fd014a3801a1bfb4007fd524b3ec2ab379a14bae
Rick Chi <wenzhongchi@gmail.com>
2025-03-27 22:04:49 -0400

    fix: update bsc rpc

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -8,6 +8,10 @@ import { ChainMap, ChainMetadata } from '@hyperlane-xyz/sdk';
 export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
   bsc: {
     ...bsc,
+    rpcUrls: [
+      { http: 'https://bnb-mainnet.g.alchemy.com/v2/zd_R_1HU96Wq3zVPDsYG_BHfObwhGYm_' },
+      ...bsc.rpcUrls,
+    ],
     mailbox: bscAddresses.mailbox,
   },
   soneium: {
