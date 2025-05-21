f1222841de6e8e331dd42a3e5715c8af5b7f5de1
Rick Chi <wenzhongchi@gmail.com>
2025-05-21 00:55:23 -0400

    fix:  wbnb and b2

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -117,6 +117,62 @@ export const warpRouteConfigs: WarpCoreConfig = {
       logoURI: 'https://img.sonex.so/bridge/here.svg',
       connections: [{ token: 'ethereum|ethereum|0x7c0c429e092Cb28fDcc3e6e67ca06D3b9fAb0306' }],
     },
+    {
+      // The ChainName of the token
+      chainName: 'bsc',
+      // See https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/token/TokenStandard.ts
+      standard: TokenStandard.EvmHypCollateral,
+      // The token metadata (decimals, symbol, name)
+      decimals: 18,
+      symbol: 'WBNB',
+      name: `Wrapped BNB`,
+      // The router address
+      addressOrDenom: '0x4fB85EA454dCe1910eB9F7aA82DAA0005De294C4',
+      // The address of the underlying collateral token
+      collateralAddressOrDenom: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      // A path to a token logo image
+      logoURI: 'https://img.sonex.so/bridge/wbnb.jpg',
+      // The list of tokens this one is connected to
+      connections: [{ token: 'ethereum|soneium|0x77dd7C53cbC53c124dCCA99ca799665d18e9cdfB' }],
+    },
+    {
+      chainName: 'soneium',
+      standard: TokenStandard.EvmHypSynthetic,
+      decimals: 18,
+      symbol: 'WBNB',
+      name: `Wrapped BNB`,
+      addressOrDenom: '0x77dd7C53cbC53c124dCCA99ca799665d18e9cdfB',
+      logoURI: 'https://img.sonex.so/bridge/wbnb.jpg',
+      connections: [{ token: 'ethereum|bsc|0x4fB85EA454dCe1910eB9F7aA82DAA0005De294C4' }],
+    },
+    {
+      // The ChainName of the token
+      chainName: 'bsc',
+      // See https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/token/TokenStandard.ts
+      standard: TokenStandard.EvmHypCollateral,
+      // The token metadata (decimals, symbol, name)
+      decimals: 18,
+      symbol: 'B2',
+      name: `BSquared Token`,
+      // The router address
+      addressOrDenom: '0xeA88E4C9E077F22CC3c23B6F2056FD848DbdEb69',
+      // The address of the underlying collateral token
+      collateralAddressOrDenom: '0x783c3f003f172c6Ac5AC700218a357d2D66Ee2a2',
+      // A path to a token logo image
+      logoURI: 'https://img.sonex.so/bridge/b2.jpg',
+      // The list of tokens this one is connected to
+      connections: [{ token: 'ethereum|soneium|0x7Dd00550d8EBa4B59445923573B3B89389A03573' }],
+    },
+    {
+      chainName: 'soneium',
+      standard: TokenStandard.EvmHypSynthetic,
+      decimals: 18,
+      symbol: 'B2',
+      name: `BSquared Token`,
+      addressOrDenom: '0x7Dd00550d8EBa4B59445923573B3B89389A03573',
+      logoURI: 'https://img.sonex.so/bridge/b2.jpg',
+      connections: [{ token: 'ethereum|bsc|0xeA88E4C9E077F22CC3c23B6F2056FD848DbdEb69' }],
+    },
   ],
   options: {},
 };
